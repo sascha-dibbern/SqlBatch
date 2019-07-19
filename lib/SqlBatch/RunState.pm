ce7b8d9c06a3774e4e6176dd8913037a75b4658e
Sascha Dibbern <sascha@dibbern.info>
2019-07-20 01:28:12 +0200

    Added runstate-class

---
+++ lib/SqlBatch/RunState.pm
@@ -0,0 +1,67 @@
+package SqlBatch::RunState;
+
+use v5.16;
+use strict;
+use warnings;
+use utf8;
+
+use Carp;
+use Data::Dumper;
+
+sub new {
+    my ($class,$copy_of)=@_;
+
+    my $self;
+
+    if (defined $copy_of) {
+	# Replicate
+	my %copy = map {
+	    # Generate new has element
+	    $_ => $copy_of->{$_}
+	} grep { 
+	    # Copy only public attributes => non-"_..."
+	    ! /^_/ 
+	} keys %$copy_of;
+
+	$self = \%copy;
+    } else {
+	# Default
+	$self = {
+	    autocommit => 1 ,
+	};
+    }
+
+    return bless $self, $class;
+}
+
+sub commit_mode {
+    my $self = shift;
+
+    if ($self->{autocommit}) {
+	return 'autocommitted';
+    } else {
+	return 'nonautocommitted'; 
+    }
+}
+
+sub AUTOLOAD {
+    my $self = shift;
+
+    my $new_value;
+    my $has_new_values = scalar(@_);
+    if ($has_new_values) {
+	$new_value = shift;
+    }
+
+    our $AUTOLOAD; # keep 'use strict' happy
+    my $attr = $AUTOLOAD;
+    $attr =~ s/.*:://;
+
+    if ($has_new_values) {
+	$self->{$attr} = $new_value;
+    }
+
+    return $self->{$attr};
+}
+
+1;
