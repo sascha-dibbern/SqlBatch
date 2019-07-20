f7d16c3526166b950855fbd2931253405256ec71
Sascha Dibbern <sascha@dibbern.info>
2019-07-20 12:35:49 +0200

    Added abstract plan class

---
+++ lib/SqlBatch/AbstractPlan.pm
@@ -0,0 +1,25 @@
+package SqlBatch::AbstractPlan;
+
+use v5.16;
+use strict;
+use warnings;
+use utf8;
+
+use Carp;
+use Data::Dumper;
+
+sub new {
+    my ($class,$config,%defaults)=@_;
+
+    my $self = \%defaults;
+
+    return bless $self, $class;
+}
+
+sub add_instructions {
+    my $self         = shift;
+
+    croak "Abstract methode";
+}
+
+1;
