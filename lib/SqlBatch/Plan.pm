0bcbd05b53470a05323e3d6ae7534b5eca0103ef
Sascha Dibbern <sascha@dibbern.info>
2019-07-20 02:12:49 +0200

    Refactoring: instruction-objects,runstate-object,instruction-addresses

--- lib/SqlBatch/Plan.pm
+++ lib/SqlBatch/Plan.pm
@@ -7,16 +7,18 @@ use utf8;
 
 use Carp;
 use Data::Dumper;
+use SqlBatch::RunState;
+use parent "SqlBatch::AbstractPlan";
 
 sub new {
     my ($class,$config)=@_;
 
-    my $self = {
+    my $self = SqlBatch::AbstractPlan->new(
 	configuration => $config,	
 	filters       => [],
 	instructions  => [],
-	commit_mode   => 'autocommitted',
-    };
+	run_state     => SqlBatch::RunState->new();,
+    );
 
     return bless $self, $class;
 }
@@ -46,149 +48,63 @@ sub add_instructions {
     } else {
 	push @{$self->{instructions}},@instructions;
     }
+
+    # Give instructions a self-awareness of their address
+    my $address = 0;
+    for my $instruction (@{$self->{instructions}}) {
+	$instruction->address($address);
+	$address++;
+    }
 }
 
 sub run {
     my $self = shift;
 
-    for my $instruction (@{$self->{instructions}}) {
-	$self->_run_instruction($instruction);
+    $self->{runstate} = SqlBatch::RunState->new();
+
+    my $instructions_count = scalar(@{$self->{instructions}};
+    $self->{former_instruction_address}  = 0;
+    $self->{current_instruction_address} = 0;
+    
+    while ($self->{current_instruction_address} < $instructions_count) {
+       
+	my $instruction = $self->{instructions}->[$self->{current_instruction_address}];
+
+	# Initialize runstate and environment for instruction before running
+	$instruction->runstate($self->{runstate});
+	$instruction->databasehandle($self->current_databasehandle());
+
+	# Run it
+	eval {
+	    $instruction->run;
+
+	    # Transport the runstate for the next instruction
+	    my $last_runstate = $instruction->runstate();	
+	    $self->{runstate} = SqlBatch::RunState->new($last_runstate);
+
+	    # Count to next instruction
+	    $self->{former_instruction_address} = $self->{current_instruction_address};
+	    $self->{current_instruction_address}++;
+	};
+	if ($@) {
+	    say STDERR "Error while executing instruction:";
+	    say STDERR " instruction type:".ref($instruction);
+	    say STDERR " file: ".$instruction->argument('file');
+	    say STDERR " line: ".$instruction->argument('line_nr');
+	    say STDERR " error: ".$@;	    
+	    say STDERR " dump: ".Dumper($instruction->state_dump);
+	    exit 1;
+	}
     }
 }
 
-sub current_dbh {
+sub current_databasehandle {
     my $self = shift;
 
     my $dbhs = $self->{configuration}->database_handles;
-    my $mode = $self->{commit_mode};
+    my $mode = $self->{runstate}->commit_mode();
 
     return $dbhs->{$mode};
-}
-
-sub warn {
-    my $self        = shift;
-    my $text        = shift;
-    my $instruction = shift;
-
-    say STDERR "WARNING: ".$text;
-    say "Instruction: ".Dumper($instruction) if defined $instruction;
-}
-
-sub _run_instruction {
-    my $self        = shift;
-    my $instruction = shift;
-
-    my $dbh = $self->current_dbh;
-#    say "%%".Dumper($instruction);
-
-    my $type    = $instruction->{type};
-    my $force_autocommit = $self->{configuration}->item('force_autocommit');
-
-    if ($type eq "SQL") {
-	$self->_run_sql_instruction($dbh,$instruction);
-    } elsif ($type eq "INSERT") {
-	$self->_run_insert_instruction($dbh,$instruction);
-    } elsif ($type eq "DELETE") {
-	$self->_run_delete_instruction($dbh,$instruction);
-    } elsif ($type eq "BEGIN") {	
-	if ($force_autocommit) {
-	    $self->warn("Explicit beginning of a transaction is not enforced due to 'force_autocommit' in configuration",$instruction);
-	} else {
-	    $self->{commit_mode} = 'nonautocommitted';
-	    $dbh->begin_work;
-	}
-    } elsif ($type eq "COMMIT") {
-	if ($force_autocommit) {
-	    $self->warn("Explicit commiting of a transaction is not enforced due to 'force_autocommit' in configuration",$instruction);
-	} else {
-	    $dbh->commit;
-	    $self->{commit_mode} = 'autocommitted';
-	}
-    } elsif ($type eq "ROLLBACK") {
-	if ($force_autocommit) {
-	    $self->warn("Explicit rollback of a transaction is not enforced due to 'force_autocommit' in configuration",$instruction);
-	} else {
-	    $dbh->rollback;
-	    $self->{commit_mode} = 'autocommitted';
-	}
-    }
-
-}
-
-sub _run_sql_instruction {
-    my $self        = shift;
-    my $dbh         = shift;
-    my $instruction = shift;
-
-    $dbh->do($instruction->{sql});
-}
-
-sub _run_insert_instruction {
-    my $self        = shift;
-    my $dbh         = shift;
-    my $instruction = shift;
-
-    my $field_values = $instruction->{data};
-    my @fields       = sort keys %$field_values;
-    my $sth          = ${$instruction->{sth_placeholder}};
-
-    unless (defined $sth) {
-	my $table = $instruction->{table};
-	my $sql   = sprintf(
-	    "insert into %s (%s) values (%s)",
-	    $table, 
-	    join(",", @fields), 
-	    join(",", ("?")x@fields)
-	    );
-
-	$sth = $dbh->prepare($sql);	    
-	${$instruction->{sth_placeholder}} = $sth;
-    }
-
-    my @values = @{$field_values}{@fields};
-    my $rv;
-    eval {
-	$rv = $sth->execute(@values);
-    };
-    if ($@) {
-	say "Failed running instruction: ".Dumper($instruction);
-	say "Error: $@";
-	croak $@;
-    }
-}
-
-sub _run_delete_instruction {
-    my $self        = shift;
-    my $dbh         = shift;
-    my $instruction = shift;
-
-    my $field_values = $instruction->{data};
-    my @fields       = sort keys %$field_values;
-    my $sth          = ${$instruction->{sth_placeholder}};
-
-    unless (defined $sth) {
-	my $table = $instruction->{table};
-	my @constraints = map { "$_=?" } @fields;
-
-	my $sql   = sprintf(
-	    "delete from %s where %s",
-	    $table,
-	    join(' and ',@constraints)
-	    );
-	$sth = $dbh->prepare($sql);	    
-	${$instruction->{sth_placeholder}} = $sth;
-    }
-
-    my @values = @{$field_values}{@fields};
-    my $rv;
-    eval {
-	$rv = $sth->execute(@values);
-    };
-    if ($@) {
-	say "Failed running instruction: "..Dumper($instruction);
-	say "Error: $@";
-	croak $@;
-    }
 }
 
 1;
